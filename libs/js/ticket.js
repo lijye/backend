776282bc8b75cc8197e3ef47865602d06df0e26b
alphaGo <artisanabc@outlook.com>
2017-09-25 16:37:25 +0800

    optimize ticket.js

--- libs/js/ticket.js
+++ libs/js/ticket.js
@@ -1,21 +1,22 @@
-console.log("ticket.js --> success");
-console.log($);
+// console.log("ticket.js --> success");
 
+// 生成ticket条目函数
 function ticketFn(el, arr) {
     var ticketContent = $("<div id='ticket-content'></div>");
     ticketContent.appendTo($(el));
 
+    // 循环生成ticket
     for (var i = 0; i < arr.length; i++) {
+        // 取值
+        var textVal = arr[i].text;
+        var descVal = arr[i].desc;
+        var contentVal = arr[i].content;
+        var dateVal = arr[i].date;
+
         // 判断类型
         if (arr[i].type == 1) {
-            // 取值
-            var typeVal = arr[i].name;
-            var textVal = arr[i].text;
-            var descVal = arr[i].desc;
-            var contentVal = arr[i].content;
-            var dateVal = arr[i].date;
 
-            // 创建一个ticket、插入
+            // 创建一个ticket条目、插入ticketContent
             var discount = $("<div class='discount'></div>");
             discount.appendTo(ticketContent);
 
@@ -24,45 +25,18 @@ function ticketFn(el, arr) {
             ticketLeft.appendTo(discount);
 
             // 创建左侧节点
-            var type = $("<p class='type'></p>");
-            var text = $("<p class='type-text'>折</p>");
-            var span = $("<span></span>")
-            var desc = $("<p class='type-desc type-desc-discount'></p>");
-            // 节点赋值
-            type.html(typeVal);
-            span.html(textVal);
-            desc.html(descVal);
-            span.prependTo(text);
+            var type = $("<p class='type'>折扣券</p>");
+            var text = $("<p class='type-text'><span>" + textVal + "</span>折</p>");
+            var desc = $("<p class='type-desc type-desc-discount'>" + descVal + "</p>");
+
+            // 插入左侧容器
             type.appendTo(ticketLeft);
             text.appendTo(ticketLeft);
             desc.appendTo(ticketLeft);
-
-            // 右侧容器
-            var ticketRight = $("<div class='ticket-right'></div>");
-            ticketRight.appendTo(discount);
-
-            // 创建右侧节点
-            var content = $("<p class='type-content'></p>");
-            var date = $("<p class='type-date'></p>");
-            var scope1 = $("<p class='type-scope1'>全国通用</p>");
-            var scope2 = $("<p class='type-scope2'>只可抵扣时间费用、里程费用</p>");
-            // 节点赋值
-            content.html(contentVal);
-            date.html(dateVal);
-            content.appendTo(ticketRight);
-            date.appendTo(ticketRight);
-            scope1.appendTo(ticketRight);
-            scope2.appendTo(ticketRight);
 
         } else if (arr[i].type == 2) {
-            // 取值
-            var typeVal = arr[i].name;
-            var textVal = arr[i].text;
-            var descVal = arr[i].desc;
-            var contentVal = arr[i].content;
-            var dateVal = arr[i].date;
 
-            // 创建一个ticket、插入
+            // 创建一个ticket条目、插入ticketContent
             var discount = $("<div class='discount'></div>");
             discount.appendTo(ticketContent);
 
@@ -71,44 +45,18 @@ function ticketFn(el, arr) {
             ticketLeft.appendTo(discount);
 
             // 创建左侧节点
-            var type = $("<p class='type'></p>");
-            var text = $("<p class='type-text'>元</p>");
-            var span = $("<span></span>")
-            var desc = $("<p class='type-desc type-desc-discount'></p>");
-            // 节点赋值
-            type.html(typeVal);
-            span.html(textVal);
-            desc.html(descVal);
-            span.prependTo(text);
+            var type = $("<p class='type'>代金券</p>");
+            var text = $("<p class='type-text'><span>" + textVal + "</span>折</p>");
+            // var desc = $("<p class='type-desc type-desc-discount'>" + descVal + "</p>");
+
+            // 插入左侧容器
             type.appendTo(ticketLeft);
             text.appendTo(ticketLeft);
-            desc.appendTo(ticketLeft);
+            // desc.appendTo(ticketLeft);
 
-            // 右侧容器
-            var ticketRight = $("<div class='ticket-right'></div>");
-            ticketRight.appendTo(discount);
-
-            // 创建右侧节点
-            var content = $("<p class='type-content'></p>");
-            var date = $("<p class='type-date'></p>");
-            var scope1 = $("<p class='type-scope1'>全国通用</p>");
-            var scope2 = $("<p class='type-scope2'>只可抵扣时间费用、里程费用</p>");
-            // 节点赋值
-            content.html(contentVal);
-            date.html(dateVal);
-            content.appendTo(ticketRight);
-            date.appendTo(ticketRight);
-            scope1.appendTo(ticketRight);
-            scope2.appendTo(ticketRight);
         } else if (arr[i].type == 3) {
-            // 取值
-            var typeVal = arr[i].name;
-            var textVal = arr[i].text;
-            var descVal = arr[i].desc;
-            var contentVal = arr[i].content;
-            var dateVal = arr[i].date;
 
-            // 创建一个ticket、插入
+            // 创建一个ticket条目、插入ticketContent
             var discount = $("<div class='discount'></div>");
             discount.appendTo(ticketContent);
 
@@ -117,33 +65,30 @@ function ticketFn(el, arr) {
             ticketLeft.appendTo(discount);
 
             // 创建左侧节点
-            var type = $("<p class='type'></p>");
-            var text = $("<p class='type-text type-text-combo'></p>");
-            var desc = $("<p class='type-desc type-desc-combo'></p>");
-            // 节点赋值
-            type.html(typeVal);
-            text.html(textVal);
-            desc.html(descVal);
+            var type = $("<p class='type'>套餐券</p>");
+            var text = $("<p class='type-text type-text-combo'>" + textVal + "</p>");
+            var desc = $("<p class='type-desc type-desc-combo'>" + descVal + "</p>");
+
+            // 插入左侧容器
             type.appendTo(ticketLeft);
             text.appendTo(ticketLeft);
             desc.appendTo(ticketLeft);
-
-            // 右侧容器
-            var ticketRight = $("<div class='ticket-right'></div>");
-            ticketRight.appendTo(discount);
-
-            // 创建右侧节点
-            var content = $("<p class='type-content'></p>");
-            var date = $("<p class='type-date'></p>");
-            var scope1 = $("<p class='type-scope1'>全国通用</p>");
-            var scope2 = $("<p class='type-scope2'>只可抵扣时间费用、里程费用</p>");
-            // 节点赋值
-            content.html(contentVal);
-            date.html(dateVal);
-            content.appendTo(ticketRight);
-            date.appendTo(ticketRight);
-            scope1.appendTo(ticketRight);
-            scope2.appendTo(ticketRight);
         }
+
+        // 右侧容器
+        var ticketRight = $("<div class='ticket-right'></div>");
+        ticketRight.appendTo(discount);
+
+        // 创建右侧节点
+        var content = $("<p class='type-content'>" + contentVal + "</p>");
+        var date = $("<p class='type-date'>" + dateVal + "</p>");
+        var scope1 = $("<p class='type-scope1'>全国通用</p>");
+        var scope2 = $("<p class='type-scope2'>只可抵扣时间费用、里程费用</p>");
+
+        // 插入右侧容器
+        content.appendTo(ticketRight);
+        date.appendTo(ticketRight);
+        scope1.appendTo(ticketRight);
+        scope2.appendTo(ticketRight);
     }
 }
